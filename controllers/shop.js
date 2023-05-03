9563275e3a0186ce4b5f3467f4741dbea7c7fb78
Rizwan Mushtaq <rizwan.mushtaq@ottonova.de>
2023-05-03 16:47:11 +0200

    added product detail page

--- controllers/shop.js
+++ controllers/shop.js
@@ -12,9 +12,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const productId = req.params.productId;
-  console.log(productId);
-
-  res.redirect("/");
+  Product.findById(productId, (product) => {
+    res.render("shop/product-detail", {
+      product,
+      pageTitle: product.title,
+      path: "/products",
+    });
+  });
 };
 
 exports.getIndex = (req, res, next) => {
